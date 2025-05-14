1aadf7472206a5bf358e8197469daafc9c1984c1
Elena Rozhkova <elenaroschkowa@gmx.de>
2025-05-14 14:42:58 +0200

    add menu new

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="flex-grow  md:overflow-y-auto sm:px-12 sm:py-2 px-0"> <Navbar /></div>
+        <div className="flex-grow container mx-auto md:overflow-y-auto  sm:py-2 px-0"> <Navbar /></div>
         <div className="container flex mx-auto h-screen flex-col md:flex-row md:overflow-hidden">
           <div className="w-full flex-none md:w-64">
             <SideNav />
